a1b44bab232a3f9cbfdf7b7c792eeb9e6bc3a05e
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2023-02-21 12:38:01 +0200

    feat: signup controllers done

--- lib/common/widgets/custom_textfield.dart
+++ lib/common/widgets/custom_textfield.dart
@@ -4,14 +4,22 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
-  const CustomTextField({super.key, required this.controller});
+  final String hinttext;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hinttext,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      key: key,
       controller: controller,
       // ignore: prefer_const_constructors
       decoration: InputDecoration(
+        hintText: hinttext,
         border: const OutlineInputBorder(
           borderSide: BorderSide(color: Colors.black),
         ),
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bronzedblue/common/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -12,6 +13,20 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _signUpFormKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _mobileNumberController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    _mobileNumberController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +41,42 @@ class _LoginScreenState extends State<LoginScreen> {
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
             ),
-            Form(
-              key: _signUpFormKey,
-              child: Column(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Form(
+                  key: _signUpFormKey,
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: _emailController,
+                        hinttext: "Enter Username",
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomTextField(
+                        controller: _emailController,
+                        hinttext: "Enter Email",
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomTextField(
+                        controller: _emailController,
+                        hinttext: "Enter Password",
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      CustomTextField(
+                        controller: _emailController,
+                        hinttext: "Enter Mobile Number",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
